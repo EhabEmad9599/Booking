c2ae263d888843d18d520bed3ab625dacfbdffe8
Ehab Emad <47368484+EhabEmad9599@users.noreply.github.com>
2020-10-10 06:46:25 +0200

    fix responsive problem

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
-// const menu = document.querySelector('.burger');
-// const line1 = document.querySelector('.line1');
-// const line2 = document.querySelector('.line2');
-// const navBar = document.querySelector('.nav-bar');
+const menu = document.querySelector('.burger');
+const line1 = document.querySelector('.line1');
+const line2 = document.querySelector('.line2');
+const navBar = document.querySelector('.nav-bar');
 
-// navToggle = () => {
-//   if (menu.classList.toggle('active')) {
-//     navBar.classList.add('open');
-//     logo.style.color = '#000';
-//     line1.style.background = '#000';
-//     line2.style.background = '#000';
-//     document.body.classList.add('hide');
-//   } else {
-//     navBar.classList.remove('open');
-//     logo.style.color = '#FFF';
-//     line1.style.background = '#FFF';
-//     line2.style.background = '#FFF';
-//     document.body.classList.remove('hide')
-//   }
-// }
+navToggle = () => {
+  if (menu.classList.toggle('active')) {
+    navBar.classList.add('open');
+    logo.style.color = '#000';
+    line1.style.background = '#000';
+    line2.style.background = '#000';
+    document.body.classList.add('hide');
+  } else {
+    navBar.classList.remove('open');
+    logo.style.color = '#FFF';
+    line1.style.background = '#FFF';
+    line2.style.background = '#FFF';
+    document.body.classList.remove('hide')
+  }
+}
 
-// menu.addEventListener('click', navToggle);
+menu.addEventListener('click', navToggle);
 
 // owl
 $(document).ready(function () {
